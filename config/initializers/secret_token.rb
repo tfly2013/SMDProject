f52d6b049cc8ff881e97372295d080b8f2e51131
tfly2013 <tfly2011@gmail.com>
2014-05-07 11:41:06 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SMDProject::Application.config.secret_key_base = 'b14520f5e33f320caa7cdb58b451cfa87360c04062db165581233e0942036fa9c028eace14090b3aa94b2f8a720eb0a076db81965b8d71698019de5fd6d19d57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SMDProject::Application.config.session_store :cookie_store, key: '_SMDProject_session'
