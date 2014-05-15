27b60de95ce047bdd15099c81845f88c5d52c165
Kimply <5321765warpten>
2014-05-15 20:07:42 +1000

    nothing else, but changed credentials

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,10 @@ class Event < ActiveRecord::Base
   has_many :pictures
   has_many :tickets
   has_many :comments
+  validates :name, length: { maximum: 30 }, presence: true, uniqueness: { case_sensitive: false }
+  validates :type, length: { maximum: 15 }, presence: true
+  validates :time, presence: true
+  validates :location, presence: true
+  validates :description, length: { maximum: 200 }
+  validates :status, presence: true
 end
