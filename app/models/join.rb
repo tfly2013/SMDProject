0478ce46d55bcae0391955f6678c5b456783bff1
Fei Tang <tfly2011@gmail.com>
2014-05-17 22:09:18 +1000

    society branch init

--- app/models/join.rb
+++ app/models/join.rb
@@ -2,7 +2,7 @@ class Join < ActiveRecord::Base
   belongs_to :member
   belongs_to :society
   accepts_nested_attributes_for :member
-
+  
   validates_uniqueness_of :member_id, :scope => :society_id
   validates :role, presence: true
 end
