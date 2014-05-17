1ae31ee6baf0faebcb8a28915b0b1f81442c80fb
Fei Tang <tfly2011@gmail.com>
2014-05-17 22:03:18 +1000

    min changes

--- app/models/join.rb
+++ app/models/join.rb
@@ -1,10 +1,8 @@
 class Join < ActiveRecord::Base
   belongs_to :member
   belongs_to :society
-  accepts_nested_attributes_for :member,
-          reject_if: proc { |attributes| attributes['email'].blank? ||
-                                         attributes['name'].blank?}
-                                         
+  accepts_nested_attributes_for :member
+
   validates_uniqueness_of :member_id, :scope => :society_id
   validates :role, presence: true
 end
