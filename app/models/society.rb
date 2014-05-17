ada771cf1a21498cb829a5f5ead58475ade3d0e8
Fei Tang <tfly2011@gmail.com>
2014-05-18 00:31:31 +1000

    Add Logo for societies

--- app/models/society.rb
+++ app/models/society.rb
@@ -9,10 +9,11 @@ class Society < ActiveRecord::Base
   accepts_nested_attributes_for :joins, allow_destroy: true,
                       reject_if: proc { |attributes| attributes['role'].blank? }
   
-  has_attached_file :logo, styles: { medium: "150x150>" }, :default_url => "/images/missing.png"  
-  validates_attachment_content_type :logo, :content_type => { :content_type => "image/png" }
+  has_attached_file :logo, styles: { medium: "200x200>" }, :default_url => "/images/missing.png" 
+  
+  validates_attachment_content_type :logo, content_type: ['image/jpeg', 'image/jpeg', 'image/png']
+  validates_attachment_size :logo, :less_than => 4.megabytes
   
   validates :name, length: { maximum: 30 }, presence: true, uniqueness: { case_sensitive: false }
   validates :register_num, length: { is: 8 }, numericality: { only_integer: true }
-  validates :description, length: { maximum: 100 }
 end
